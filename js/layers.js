7001e467c68352ddb4c7f737f3f5ce34634524f6
chenpengda <61399296+chenpengda@users.noreply.github.com>
2022-04-03 18:52:41 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("t", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#66ccff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "thoughts", // Name of prestige currency
     baseResource: "illusions", // Name of resource prestige is based on

a4a07c454630b5902e430e434292c7bb0e8b50b5
chenpengda <61399296+chenpengda@users.noreply.github.com>
2022-04-03 20:01:18 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -10,9 +10,9 @@ addLayer("t", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "thoughts", // Name of prestige currency
     baseResource: "illusions", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.points*0.05}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.0, // Prestige currency exponent
+    exponent: 0.8, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
